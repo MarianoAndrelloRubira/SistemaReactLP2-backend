06a8264a556a13b9a98a9acf89c5080b9690632c
MarianoAndrelloRubira <mariano_rubira2020@hotmail.com>
2023-10-30 17:49:09 -0300

    coreção nas intruções sql

--- percistencia/intrucoes.sql
+++ percistencia/intrucoes.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE sistema;
 
-USE sistema:
+USE sistema;
 
 CREATE TABLE categoria(
     cat_codCat INT NOT NULL AUTO_INCREMENT,
-    cat_desCat VARCHAR(100) NOT NULL
+    cat_desCat VARCHAR(100) NOT NULL,
 
     CONSTRAINT pk_categoria PRIMARY KEY(cat_codCat) 
 );
@@ -18,7 +18,7 @@ CREATE TABLE produto(
     prod_precLot DECIMAL(10,2) NOT NULL,
     prod_precVenda DECIMAL(10,2) NOT NULL,
     cat_codCat INT NOT NULL,
-    prod_data DATE NOT NULL
+    prod_data DATE NOT NULL,
 
     CONSTRAINT pk_produto PRIMARY KEY(prod_cod),
     CONSTRAINT fk_categoria FOREIGN KEY(cat_codCat) REFERENCES categoria(cat_codCat)
